a259bda1c0c337d7edb5863329d88085ffe2a84e
Moises Elias RC <141798007+Sesiom31@users.noreply.github.com>
2024-12-23 22:18:09 -0500

    Solve styling overflow, command start server production, and add env for production

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const URI_DB = process.env.URI_DB;
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 5000;
 export const SECRET_JWT = process.env.SECRET_JWT;
